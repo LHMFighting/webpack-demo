4c0de493d931d5ff8dd405dcabc3a932671df753
LHM <2585283373@qq.com>
2018-02-28 16:28:09 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ module.exports = {
     // entry: ["./src/script/main.js","./src/script/a.js"], // 2.array
     entry: {
         main: "./src/script/main.js",
-        a: "./src/script/a.js"
+        a: "./src/script/a.js",
+        b: "./src/script/b.js",
+        c: "./src/script/c.js"
     }, // 3.object
     // 这里应用程序开始执行
     // webpack 开始打包
@@ -14,16 +16,38 @@ module.exports = {
     output: {
         // webpack 如何输出结果的相关选项
 
-        path: path.resolve(__dirname, "dist"), // string
+        path: path.resolve(__dirname, "./dist"), // string
         // 所有输出文件的目标路径
         // 必须是绝对路径（使用 Node.js 的 path 模块）
 
         // filename: "bundle.js"  // 单个入口
-        filename: "[name]-[hash].js" // 多个入口
+        filename: "js/[name]-[hash].js", // 多个入口
         // 「入口分块(entry chunk)」的文件名模板（出口分块？）
+
+        publicPath: "http://cdn.com/"
 
     },
     plugins: [
-        new HtmlWebpackPlugin({template: 'index.html'})
+        new HtmlWebpackPlugin({
+            filename: 'a.html',
+            template: 'index.html',
+            inject: 'body',
+            title: 'this is a.html',
+            chunks: ['main', 'a']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'b.html',
+            template: 'index.html',
+            inject: 'body',
+            title: 'this is b.html',
+            chunks: ['main','b']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'c.html',
+            template: 'index.html',
+            inject: 'body',
+            title: 'this is c.html',
+            chunks: ['c']
+        })
     ]
 };
